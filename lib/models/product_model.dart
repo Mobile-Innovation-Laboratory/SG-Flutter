938ebdc02ac18bea5d0ef7045750f31bc3d86579
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-12-16 11:02:37 +0700

    refactor: add attribute

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -1,18 +1,19 @@
 class ProductModel {
   String id;
   String image;
-  String title;
+  String name;
   double price;
   String description;
+  String type;
   bool isFavorite;
 
   ProductModel({
     required this.id,
     required this.image,
-    required this.title,
+    required this.name,
     required this.price,
     required this.description,
+    required this.type,
     required this.isFavorite,
   });
 }
-
